8279daad494a6b92c31f8100e0ad29e9e4e6dab2
andre-barrera <andre.barrera.99@gmail.com>
2026-01-26 20:13:00 -0600

    updated changes

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const mongodb = require("./data/database.js");
+const mongodb = require('./data/database');
 
 const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
-app.use("/", require("./routes"));
+app.use('/', require('./routes'));
+
 
 mongodb.initDb( (err) => {
-    if (err) {
+    if(err){
         console.log(err);
     }
     else {
-        app.listen(port, () => {
-            console.log(`Data base is listening and running in port ${port}`);
-        })
+        app.listen(port, () => {console.log(`Database is listening and node running port ${port}`)});
     }
 });
 
