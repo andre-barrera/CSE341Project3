478e9d83106bfd47e8afb3d3e720e7d2221b24a4
andre-barrera <andre.barrera.99@gmail.com>
2026-01-26 20:26:58 -0600

    updated changes

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const mongodb = require("./data/database.js");
+const mongodb = require('./data/database');
 
 const port = process.env.PORT || 3000;
 
-app.use("/", require("./routes"));
+app.use(express.json());
 
-mongodb.initDb( (err) => {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        app.listen(port, () => {
-            console.log(`Data base is listening and running in port ${port}`);
-        })
-    }
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
+app.use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () =>
+      console.log(`Server running on port ${port}`)
+    );
+  }
 });
 
 
