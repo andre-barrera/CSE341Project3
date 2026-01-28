80357401c37450477ca8f1de06d193333b316fe6
andre-barrera <andre.barrera.99@gmail.com>
2026-01-27 18:09:13 -0600

    updated changes

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,22 +1,7 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger.json');
-
-
-router.use('/api-docs', (req, res, next) => {
-  try {
-    swaggerUi.serve(req, res, next);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to load Swagger UI' });
-  }
-});
-
-router.get('/api-docs', (req, res) => {
-  try {
-    swaggerUi.setup(swaggerDocument)(req, res);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to load Swagger documentation' });
-  }
-});
+const swaggerDocument = require('./../swagger.json');
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
